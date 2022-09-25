8174060c48a4a4368fe4e472274ab32eca90f881
Shabnum Arif <shabnumarif9@gmail.com>
2022-09-25 16:47:38 +0530

    to load 404 page

--- school_admin/urls.py
+++ school_admin/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns=[
     path('',views.school_home),
+    path('add-staff',views.add_staff),
+    path('view-student',views.view_student),
+    path('view-staff',views.view_staff),
 ]
--- school_admin/views.py
+++ school_admin/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def school_home(request):
-    return render(request,'school_admin_templates/home.html')
+    return render(request,'school_admin_templates/home.html'),
+def add_staff(request):
+     return render(request,'school_admin_templates/add_staff.html'),
+def view_staff(request):
+     return render(request,'school_admin_templates/view_staff.html'),
+def view_student(request):
+     return render(request,'school_admin_templates/view_student.html'),
+
+
