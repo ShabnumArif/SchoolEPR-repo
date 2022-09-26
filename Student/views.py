a8fbbc1743cb6b3dd70d8839115b3ccdafbd381e
Shabnum Arif <shabnumarif9@gmail.com>
2022-09-26 09:26:09 +0530

    corrected path

--- Student/views.py
+++ Student/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def change_password(request):
-     return render (request,'student_templates/change_password')
+     return render (request,'student_templates/change_password.html')
 def profile(request):
-    return render(request,'student_templates/profile')
+    return render(request,'student_templates/profile.html')
 def student_home(request):
-    return render(request,'student_templates/student_home')
+    return render(request,'student_templates/student_home.html')
