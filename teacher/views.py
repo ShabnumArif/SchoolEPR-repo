03a10caab3c280554e05f4e2e12f37ef91c00f19
Shabnum Arif <shabnumarif9@gmail.com>
2022-09-26 12:50:04 +0530

    teacher- changed

--- teacher/views.py
+++ teacher/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def add_student(request):
-     return render (request, 'teacher_templates/add_student.html')
+     return render (request, 'teachers_templates/add_student.html')
 def change_password(request):
-     return render (request,'teacher_templates/change_password.html')
+     return render (request, 'teachers_templates/change_password.html')
 def teacher_home(request):
-     return render (request, 'teacher_templates/teacher_home.html')
+     return render (request, 'teachers_templates/teacher_home.html')
 def view_student(request):
-     return render (request, 'teacher_templates/view_student.html')
+     return render (request, 'teachers_templates/view_student.html')
+     
